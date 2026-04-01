5ce6862d9dedfc18cf67b64009edb302824390f7
ryanjlind <ryanjameslind@gmail.com>
2026-03-31 20:34:45 -0700

    fix js

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -112,7 +112,7 @@ export async function submitGuess() {
 
     guessBtn.disabled = true;
     guessInput.disabled = true;
-
+    const guess = getGuessValue();
     try {        
         const { data } = await submitGuessRequest(guess, gameState.currentRound);
 
