4420ed72c2306704385423eee7816e60e5397ef7
ryanjlind <ryanjameslind@gmail.com>
2026-04-04 15:00:47 -0700

    fix import

--- app/static/js/ui.js
+++ app/static/js/ui.js
@@ -1,5 +1,4 @@
 import { numberFmt, escapeHtml, parseFormattedInt } from './utils.js';
-import { resolveConflict } from './auth.js'
 
 export function setMetaError(message) {
     document.getElementById('meta').innerHTML = `<div class="value">${escapeHtml(message)}</div>`;
