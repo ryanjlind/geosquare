c38829fee06b2ffea078c891c9e65d7b9e5439b3
ryanjlind <ryanjameslind@gmail.com>
2026-08-29 10:04:07 -0700

    cache revalidate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import logging
 import os
-from flask import Flask
+from flask import Flask, Response, request
 from app.routes.daily_dashboard import daily_dashboard_bp
 from app.routes.main import main_bp
 from app.routes.profile import profile_bp
 from app.routes.weekly_e2e import weekly_e2e_bp
+
 
 def create_app() -> Flask:
     app = Flask(
@@ -22,6 +23,12 @@ def create_app() -> Flask:
     logging.getLogger('azure.monitor.opentelemetry.exporter.export._base').setLevel(logging.WARNING)
     logging.getLogger('geosquare').setLevel(logging.INFO)
     app.logger.setLevel(logging.INFO)
+
+    @app.after_request
+    def require_static_asset_revalidation(response: Response) -> Response:
+        if request.path.startswith(f'{app.static_url_path}/'):
+            response.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate'
+        return response
 
     app.register_blueprint(daily_dashboard_bp)
     app.register_blueprint(main_bp)
